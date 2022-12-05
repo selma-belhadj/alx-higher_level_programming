a7690934b113cee92058ffcfe77e74929d4c1c48
selma <belhadjselma1@gmail.com>
2022-12-05 23:37:32 +0100

    Write a function in C that checks if a singly linked list is a palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - determine if singly linked list is palindrome
+ * @head: pointer to head of singly linked list
+ * Return: 0 if not, 1 if palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *tmp = *head;
+	unsigned int size = 0, i = 0;
+	int data[10240];
+
+	if (head == NULL) /* non-existing list is not */
+		return (0);
+
+	if (*head == NULL) /* empty list is palindrome */
+		return (1);
+
+	while (tmp) /* find size of linked list */
+	{
+		tmp = tmp->next;
+		size += 1;
+	}
+	if (size == 1) /* single node list is palindrome */
+		return (1);
+
+	tmp = *head;
+	while (tmp) /* pull node data into array to compare */
+	{
+		data[i++] = tmp->n;
+		tmp = tmp->next;
+	}
+
+	for (i = 0; i <= (size / 2); i++)
+	{
+		if (data[i] != data[size - i - 1])
+			return (0);
+	}
+	return (1);
+}
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,18 +1,20 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for project
+ * for Holberton project
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
