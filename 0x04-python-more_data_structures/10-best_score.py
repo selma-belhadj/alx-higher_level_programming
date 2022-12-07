12cc79a65922372124cf7fcdda832de7e89e749a
selma <belhadjselma1@gmail.com>
2022-12-07 20:48:21 +0100

    Create a function that converts a Roman numeral to an integer

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        return(max(a_dictionary, key=a_dictionary.get))
+        return (max(a_dictionary, key=a_dictionary.get))
