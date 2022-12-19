5fe76b8f2d18203cb240f36e500e5228241d2a5a
selma <belhadjselma1@gmail.com>
2022-12-19 17:07:27 +0100

    Write a function that prints x elements of a list

--- 0x05-python-exceptions/101-main.py
+++ 0x05-python-exceptions/101-main.py
@@ -5,6 +5,7 @@ safe_function = __import__('101-safe_function').safe_function
 def my_div(a, b):
     return a / b
 
+
 result = safe_function(my_div, 10, 2)
 print("result of my_div: {}".format(result))
 
@@ -18,6 +19,7 @@ def print_list(my_list, len):
         print(my_list[i])
         i += 1
     return len
+
 
 result = safe_function(print_list, [1, 2, 3, 4], 10)
 print("result of print_list: {}".format(result))
