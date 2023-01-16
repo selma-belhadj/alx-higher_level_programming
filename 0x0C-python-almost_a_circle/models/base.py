7f1da796a571f18b0aa6117d3178af5c15eb04c6
selma <belhadjselma1@gmail.com>
2023-01-16 16:58:33 +0100

    Created the Base class xhich will be the "base" of all other classes in this project

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a base model class."""
+import json
+import csv
+import turtle
+
+
+class Base:
+    """Represent the base model.
+    Represents the "base" for all other classes in project 0x0C*.
+    Attributes:
+        __nb_objects (int): The number of instantiated Bases.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new Base.
+        Args:
+            id (int): The identity of the new Base.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
