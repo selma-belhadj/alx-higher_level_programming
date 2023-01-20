1bd4e8aab3545061caa0fa73fb81e3cbf48b45d1
selma <belhadjselma1@gmail.com>
2023-01-21 00:16:43 +0100

    Update the class Rectangle by adding the public method def display(self)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -94,4 +94,48 @@ class Rectangle(Base):
                 [print(" ", end="") for x in range(self.x)]
                 [print("#", end="") for w in range(self.width)]
                 print("")
-            
+
+        def update(self, *args, **kwargs):
+            """Update the Rectangle.
+            Args:
+                *args (ints): New attribute values.
+                    - 1st argument represents id attribute
+                    - 2nd argument represents width attribute
+                    - 3rd argument represent height attribute
+                    - 4th argument represents x attribute
+                    - 5th argument represents y attribute
+                **kwargs (dict): New key/value pairs of attributes.
+            """
+            if args and len(args) != 0:
+                a = 0
+                for arg in args:
+                    if a == 0:
+                        if arg is None:
+                            self.__init__(self.width, self.height, self.x, self.y)
+                        else:
+                            self.id = arg
+                    elif a == 1:
+                        self.width = arg
+                    elif a == 2:
+                        self.height = arg
+                    elif a == 3:
+                        self.x = arg
+                    elif a == 4:
+                        self.y = arg
+                    a += 1
+
+            elif kwargs and len(kwargs) != 0:
+                for k, v in kwargs.items():
+                    if k == "id":
+                        if v is None:
+                            self.__init__(self.width, self.height, self.x, self.y)
+                        else:
+                            self.id = v
+                    elif k == "width":
+                        self.width = v
+                    elif k == "height":
+                        self.height = v
+                    elif k == "x":
+                        self.x = v
+                    elif k == "y":
+                        self.y = v
