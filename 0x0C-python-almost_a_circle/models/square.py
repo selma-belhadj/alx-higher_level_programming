3d03cf534045d604a58593c262234eb7a3c2b67e
selma <belhadjselma1@gmail.com>
2023-01-21 00:25:15 +0100

    Update the class Square by adding the public method def to_dictionary(self) and the __str__ method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -52,15 +52,29 @@ class Square(Rectangle):
                         self.y = arg
                     a += 1
             elif kwargs and len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "id":
-                    if v is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = v
-                elif k == "size":
-                    self.size = v
-                elif k == "x":
-                    self.x = v
-                elif k == "y":
-                    self.y = v
+                for k, v in kwargs.items():
+                    if k == "id":
+                        if v is None:
+                            self.__init__(self.size, self.x, self.y)
+                        else:
+                            self.id = v
+                    elif k == "size":
+                        self.size = v
+                    elif k == "x":
+                        self.x = v
+                    elif k == "y":
+                        self.y = v
+
+        def to_dictionary(self):
+            """Return the dictionary representation of the Square."""
+            return {
+                "id": self.id,
+                "size": self.width,
+                "x": self.x,
+                "y": self.y
+            }
+
+        def __str__(self):
+            """Return the print() and str() representation of a Square."""
+            return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                    self.width)
