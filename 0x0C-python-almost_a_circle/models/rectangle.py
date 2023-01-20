f0808cb8c232c5254e3df8039c5b8746db3fd090
selma <belhadjselma1@gmail.com>
2023-01-21 00:14:59 +0100

    Update the class Rectangle by adding the public method def display(self)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,3 +82,16 @@ class Rectangle(Base):
         def area(self):
             """Return the area of the Rectangle."""
             return self.width * self.height
+
+        def display(self):
+            """Print the Rectangle using the `#` character."""
+            if self.width == 0 or self.height == 0:
+                print("")
+                return
+
+            [print("") for y in range(self.y)]
+            for h in range(self.height):
+                [print(" ", end="") for x in range(self.x)]
+                [print("#", end="") for w in range(self.width)]
+                print("")
+            
