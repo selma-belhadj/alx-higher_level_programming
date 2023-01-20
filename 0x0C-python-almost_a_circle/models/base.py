c2b83cd7418ea824d5428b95b6b030ba93ab356c
selma <belhadjselma1@gmail.com>
2023-01-20 23:43:54 +0100

    Update the class Base by adding the class method def create(cls, **dictionary)

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -53,7 +53,7 @@ class Base:
         def from_json_string(json_string):
             """Return the deserialization of a JSON string.
             Args:
-                json_string (str): A JSON str representation of a list of dicts.
+                json_string(str): A JSON str representation of a list of dicts.
             Returns:
                 If json_string is None or empty - an empty list.
                 Otherwise - the Python list represented by json_string.
@@ -61,3 +61,17 @@ class Base:
             if json_string is None or json_string == "[]":
                 return []
             return json.loads(json_string)
+
+        @classmethod
+        def create(cls, **dictionary):
+            """Return a class instantied from a dictionary of attributes.
+            Args:
+                **dictionary(dict): Key/value pairs of attributes to initialize.
+            """
+            if dictionary and dictionary != {}:
+                if cls.__name__ == "Rectangle":
+                    new = cls(1, 1)
+                else:
+                    new = cls(1)
+                new.update(**dictionary)
+                return new
