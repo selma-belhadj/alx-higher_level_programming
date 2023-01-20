bf77add542d323b68ebc8caad6890943a2788cde
selma <belhadjselma1@gmail.com>
2023-01-20 23:40:37 +0100

    Update the class Base by adding the class method def save_to_file(cls, list_objs)

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -34,3 +34,17 @@ class Base:
             if list_dictionaries is None or list_dictionaries == []:
                 return "[]"
             return json.dumps(list_dictionaries)
+
+        @classmethod
+        def save_to_file(cls, list_objs):
+            """Write the JSON serialization of a list of objects to a file.
+            Args:
+                list_objs (list): A list of inherited Base instances.
+            """
+            filename = cls.__name__ + ".json"
+            with open(filename, "w") as jsonfile:
+                if list_objs is None:
+                    jsonfile.write("[]")
+                else:
+                    list_dicts = [o.to_dictionary() for o in list_objs]
+                    jsonfile.write(Base.to_json_string(list_dicts))
