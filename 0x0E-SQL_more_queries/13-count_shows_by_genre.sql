0d16c43b71a820028a4fa78d8d98698eb70b5c68
selma <belhadjselma1@gmail.com>
2023-02-16 16:46:35 +0100

    Write a script that lists all shows from hbtn_0d_tvshows_rate by their rating

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,10 +4,10 @@
 -- Does not display genres without linked shows.
 -- Records are ordered by descending number of shows linked.
 
-SELECT tv_genres.name AS genre, 
-      COUNT(*) AS number_shows
-  FROM tv_genres
-      INNER JOIN tv_show_genres
-      ON tv_genres.id = tv_show_genres.genre_id
-  GROUP BY genre
-  ORDER BY number_shows DESC;
+SELECT g.`name` AS `genre`,
+       COUNT(*) AS `number_of_shows`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS t
+       ON g.`id` = t.`genre_id`
+ GROUP BY g.`name`
+ ORDER BY `number_of_shows` DESC;
