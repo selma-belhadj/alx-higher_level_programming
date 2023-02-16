d7f7925953e287df86280af1a457f2d85cb4cc67
selma <belhadjselma1@gmail.com>
2023-02-16 15:51:33 +0100

    Write a script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,7 +5,7 @@
 -- Records are ordered by descending number of shows linked.
 
 SELECT tv_genres.name AS genre, 
-      COUNT(*) AS 'number_shows'
+      COUNT(*) AS number_shows
   FROM tv_genres
       INNER JOIN tv_show_genres
       ON tv_genres.id = tv_show_genres.genre_id
