c7733f161918e2f928a4ee81566081ccaa2582ca
selma <belhadjselma1@gmail.com>
2023-03-22 09:24:28 +0100

    Fix the semistandard validation

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const c = 'C is fun\n'
-const python = 'Python is cool\n'
-const javascript = 'JavaScript is amazing'
-console.log(c + python + javascript)
+const c = 'C is fun\n';
+const python = 'Python is cool\n';
+const javascript = 'JavaScript is amazing';
+console.log(c + python + javascript);
