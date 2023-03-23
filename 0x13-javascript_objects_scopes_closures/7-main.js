93756059a307ec9cab23d87c57f67237d0123c6d
selma <belhadjselma1@gmail.com>
2023-03-23 16:12:33 +0100

    Write a function that returns the number of occurrences in a list: Prototype: exports.nbOccurences = function (list, searchElement)

--- 0x13-javascript_objects_scopes_closures/7-main.js
+++ 0x13-javascript_objects_scopes_closures/7-main.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 const nbOccurences = require('./7-occurrences').nbOccurences;
 
 console.log(nbOccurences([1, 2, 3, 4, 5, 6], 3));
---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+exports.nbOccurences = function (list, searchElement) {
+  let cpt = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      cpt++;
+    }
+  }
+  return cpt;
+};
