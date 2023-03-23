1d7e5d4aec10bb6cd286af139f0a307b925480a4
selma <belhadjselma1@gmail.com>
2023-03-23 15:47:39 +0100

    Write a class Rectangle that defines a rectangle he constructor must take 2 arguments w and h Initialize the instance attribute width with the value of w Initialize the instance attribute height with the value of h If w or h is equal to 0 or not a positive integer, create an empty object Create an instance method called print() that prints the rectangle using the character X Create an instance method called rotate() that exchanges the width and the height of the rectangle Create an instance method called double() that multiples the width and the height of the rectangle by 2

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let output = '';
+    for (let i = 0; i < this.width; i++) {
+      output += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(output);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+};
