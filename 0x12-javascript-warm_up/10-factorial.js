462f07e6f6e5903d4b74e65459683b49e22a9940
selma <belhadjselma1@gmail.com>
2023-03-23 12:25:56 +0100

    Write a script that computes and prints a factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,17 @@
-#!/usr/bin/env node
+#!/usr/bin/node
+
+function factorial (a) {
+  if (a === 0) {
+    return 1;
+  } else {
+    return a * factorial(a - 1);
+  }
+}
+const argsCount = process.argv.length - 2;
+
+if (argsCount === 0) {
+  console.log('1');
+} else {
+  const a = parseInt(process.argv[2]);
+  console.log(factorial(a));
+}
