364ba3058877d1b9c1b8db66d8ee2b365028b59b
selma <belhadjselma1@gmail.com>
2023-03-23 12:14:26 +0100

    Write a script that prints x times “C is fun”
    and Write a script that prints a square

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,13 +1,11 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 const argsCount = process.argv.length - 2;
 
-if (argsCount === 0) {
+if (argsCount === 0 || isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
-} else if (isNaN(process.argv[2])|| process.argv[2] <= 0 ) {
-  return;
 } else {
-    for (let i = 0; i < parseInt(process.argv[2], 10); i++) {
-        console.log('C is fun');
-      }
+  for (let i = 0; i < parseInt(process.argv[2], 10); i++) {
+    console.log('C is fun');
+  }
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,15 @@
 #!/usr/bin/env node
+
+const argsCount = process.argv.length - 2;
+
+if (argsCount === 0 || isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  let output = '';
+  for (let i = 0; i < parseInt(process.argv[2], 10); i++) {
+    output += 'X';
+  }
+  for (let i = 0; i < parseInt(process.argv[2], 10); i++) {
+    console.log(output);
+  }
+}
