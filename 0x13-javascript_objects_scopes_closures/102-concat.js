2f5864b28e0c16fc7aa3a56c62e20f1d8d169f60
selma <belhadjselma1@gmail.com>
2023-03-29 11:47:31 +0100

    Write a script that concats 2 files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+const argsCount = process.argv.length - 2;
+
+if (argsCount < 3) {
+  console.log('Requirements 3 files please');
+} else {
+  const fileA = process.argv[2];
+  const fileB = process.argv[3];
+  const fileC = process.argv[4];
+
+  ConcatTwoFiles(fileA, fileB, fileC);
+}
+
+function ConcatTwoFiles (fileOne, fileTwo, fileThree) {
+  fs.readFile(fileOne, 'utf8', (err, data1) => {
+    if (err) throw err;
+
+    fs.readFile(fileTwo, 'utf8', (err, data2) => {
+      if (err) throw err;
+
+      const concatenated = data1 + '\n' + data2;
+      fs.writeFile(fileThree, concatenated, 'utf8', (err) => {
+        if (err) throw err;
+      });
+    });
+  });
+}
