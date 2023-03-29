bf2711e2403e676e5eabdd14b95e3bedad2db0c1
selma <belhadjselma1@gmail.com>
2023-03-29 11:25:41 +0100

    Write a script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+
+function sortedOccurences (dict) {
+  const newDic = {};
+  for (const key in dict) {
+    if (newDic[dict[key]] === undefined) {
+      newDic[dict[key]] = [];
+    }
+    newDic[dict[key]].push(key);
+  }
+  console.log(newDic);
+}
+sortedOccurences(dict);
